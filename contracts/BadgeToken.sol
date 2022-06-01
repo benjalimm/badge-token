93677012e7b13f6c8a232dcdc858615ed6737ca9
Benjamin Lim <keymote@gmail.com>
2022-06-01 00:02:00 -0700

    First draft at Badge token ERC721 self implementation

--- contracts/BadgeToken.sol
+++ contracts/BadgeToken.sol
@@ -1,14 +1,34 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "../interfaces/IBadgeToken.sol";
 import "../interfaces/IBadgeRegistry.sol";
 import "../interfaces/IEntity.sol";
 import "../interfaces/IBadgeRecoveryOracle.sol";
 
-contract BadgeToken is ERC721URIStorage, IBadgeToken {
+contract BadgeToken is ERC165, IERC721, IERC721Metadata, IBadgeToken {
     using Counters for Counters.Counter;
+
+    // Token name
+    string private _name;
+
+    // Token symbol
+    string private _symbol;
+
+    // Mapping from token ID to owner address
+    mapping(uint256 => address) private _owners;
+
+    // Mapping owner address to token count
+    mapping(address => uint256) private _balances;
+
+    // Mapping for token URI
+    mapping(uint256 => string) private _tokenURIs;
+
+    // Token id
     Counters.Counter private _tokenIds;
 
     // Mapping tokenId to time minted
@@ -20,10 +40,223 @@ contract BadgeToken is ERC721URIStorage, IBadgeToken {
     constructor(
         address _entity,
         address _recoveryOracle,
-        string memory _name
-    ) ERC721(concat(_name, " - Badges"), "BADGE") {
+        string memory name_
+    ) {
+        _name = concat(name_, " - Badges");
+        _symbol = "BADGE";
         entity = _entity;
         recoveryOracle = _recoveryOracle;
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC165, IERC165)
+        returns (bool)
+    {
+        return
+            interfaceId == type(IERC721).interfaceId ||
+            interfaceId == type(IERC721Metadata).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev See {IERC721-balanceOf}.
+     */
+    function balanceOf(address owner)
+        public
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        require(
+            owner != address(0),
+            "ERC721: balance query for the zero address"
+        );
+        return _balances[owner];
+    }
+
+    /**
+     * @dev See {IERC721-ownerOf}.
+     */
+    function ownerOf(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (address)
+    {
+        address owner = _owners[tokenId];
+        require(
+            owner != address(0),
+            "ERC721: owner query for nonexistent token"
+        );
+        return owner;
+    }
+
+    /**
+     * @dev Returns whether `tokenId` exists.
+     *
+     * Tokens can be managed by their owner or approved accounts via {approve} or {setApprovalForAll}.
+     *
+     * Tokens start existing when they are minted (`_mint`),
+     * and stop existing when they are burned (`_burn`).
+     */
+    function _exists(uint256 tokenId) internal view virtual returns (bool) {
+        return _owners[tokenId] != address(0);
+    }
+
+    /**
+     * @dev See {IERC721Metadata-name}.
+     */
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-symbol}.
+     */
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        return _tokenURIs[tokenId];
+    }
+
+    /**
+     * @dev See {IERC721-approve}.
+     */
+    function approve(address to, uint256 tokenId) public virtual override {
+        revert Blocked("Badges are non-transferable");
+    }
+
+    /**
+     * @dev See {IERC721-getApproved}.
+     */
+    function getApproved(uint256 tokenId)
+        public
+        view
+        override
+        returns (address)
+    {
+        revert Blocked("Badges are non-transferable");
+    }
+
+    /**
+     * @dev See {IERC721-setApprovalForAll}.
+     */
+    function setApprovalForAll(address operator, bool approved)
+        public
+        override
+    {
+        revert Blocked("Badges are non-transferable");
+    }
+
+    /**
+     * @dev See {IERC721-isApprovedForAll}.
+     */
+    function isApprovedForAll(address owner, address operator)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        return false;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override {
+        revert Blocked("Badges are non-transferable");
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata _data
+    ) public override {
+        revert Blocked("Badges are non-transferable");
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override {
+        revert Blocked("Badges are non-transferable");
+    }
+
+    function _burn(uint256 tokenId) internal virtual {
+        address owner = ownerOf(tokenId);
+
+        _balances[owner] -= 1;
+        delete _owners[tokenId];
+
+        emit Transfer(owner, address(0), tokenId);
+    }
+
+    /**
+     * @dev Mints `tokenId` and transfers it to `to`.
+     *
+     * WARNING: Usage of this method is discouraged, use {_safeMint} whenever possible
+     *
+     * Requirements:
+     *
+     * - `tokenId` must not exist.
+     * - `to` cannot be the zero address.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _mint(address to, uint256 tokenId) internal virtual {
+        require(to != address(0), "ERC721: mint to the zero address");
+        require(!_exists(tokenId), "ERC721: token already minted");
+
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(address(0), to, tokenId);
+    }
+
+    /**
+     * @dev Sets `_tokenURI` as the tokenURI of `tokenId`.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+        internal
+        virtual
+    {
+        require(
+            _exists(tokenId),
+            "ERC721URIStorage: URI set of nonexistent token"
+        );
+        _tokenURIs[tokenId] = _tokenURI;
     }
 
     modifier entityOnly() {
@@ -37,23 +270,6 @@ contract BadgeToken is ERC721URIStorage, IBadgeToken {
         returns (string memory)
     {
         return string(abi.encodePacked(s1, s2));
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public override {
-        revert TransferBlocked();
-    }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory _data
-    ) public override {
-        revert TransferBlocked();
     }
 
     function burnWithPrejudice(uint256 tokenId) external payable override {
@@ -111,10 +327,14 @@ contract BadgeToken is ERC721URIStorage, IBadgeToken {
         for (i = i; i < ids.length; i++) {
             uint256 id = ids[i];
 
-            if (ownerOf(id) == from) {
-                _transfer(from, msg.sender, id);
-                recoveredIds[i] = id;
-            }
+            if (ownerOf(id) != from)
+                revert Unauthorized("Badge not owned by address");
+
+            _balances[from] -= 1;
+            _balances[msg.sender] += 1;
+            _owners[id] = msg.sender;
+
+            emit Transfer(from, msg.sender, id);
         }
         emit RecoveryComplete(recoveredIds, msg.sender, recoveryAddress);
     }
